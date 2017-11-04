295dbfc1575436669c55900c989a438cb0c0d364
Łukasz Chruściel <lchrusciel@gmail.com>
2017-11-04 19:59:28 +0100

    [Maintenance] Finalize kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
-class Kernel extends BaseKernel
+final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 

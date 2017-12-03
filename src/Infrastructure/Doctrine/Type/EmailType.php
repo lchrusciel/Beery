ed75c1656b64b37a8d32e946c14145fcf6950dd5
Łukasz Chruściel <lchrusciel@gmail.com>
2017-12-03 12:58:44 +0100

    [Infrastructure] [Domain] Add doctrine mappings for VO

---
+++ src/Infrastructure/Doctrine/Type/EmailType.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Doctrine\Type;
+
+use App\Domain\Model\Email;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Type;
+
+final class EmailType extends Type
+{
+    private const NAME = 'email';
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform): string
+    {
+        return $platform->getVarcharTypeDeclarationSQL($fieldDeclaration);
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        if ($value instanceof Email) {
+            return $value->value();
+        }
+
+        throw ConversionException::conversionFailed($value, static::NAME);
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?Email
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        if ($value instanceof Email) {
+            return $value;
+        }
+
+        try {
+            return new Email($value);
+        } catch (\InvalidArgumentException $e) {
+            throw ConversionException::conversionFailed($value, static::NAME);
+        }
+    }
+
+    public function getName(): string
+    {
+        return static::NAME;
+    }
+}
---
+++ src/Infrastructure/Doctrine/Type/NameType.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Doctrine\Type;
+
+use App\Domain\Model\Name;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Type;
+
+final class NameType extends Type
+{
+    private const NAME = 'name';
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform): string
+    {
+        return $platform->getVarcharTypeDeclarationSQL($fieldDeclaration);
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        if ($value instanceof Name) {
+            return $value->value();
+        }
+
+        throw ConversionException::conversionFailed($value, static::NAME);
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?Name
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        if ($value instanceof Name) {
+            return $value;
+        }
+
+        try {
+            return new Name($value);
+        } catch (\InvalidArgumentException $e) {
+            throw ConversionException::conversionFailed($value, static::NAME);
+        }
+    }
+
+    public function getName(): string
+    {
+        return static::NAME;
+    }
+}

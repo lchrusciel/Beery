5cb06d16dec98814877d58ed6e33e4190dc5a93a
Łukasz Chruściel <lchrusciel@gmail.com>
2017-12-26 13:06:19 +0100

    [Maintenance] Spec cleanup

--- spec/Application/Command/AddBeerSpec.php
+++ spec/Application/Command/AddBeerSpec.php
@@ -11,23 +11,16 @@ use Prooph\Common\Messaging\Command;
 
 final class AddBeerSpec extends ObjectBehavior
 {
-    function let(): void
+    function it_represents_add_beer_intention(): void
     {
         $this->beConstructedThrough('create', [new Name('King of Hop'), new Abv(5)]);
+
+        $this->name()->shouldBeLike(new Name('King of Hop'));
+        $this->abv()->shouldBeLike(new Abv(5));
     }
 
     function it_is_command(): void
     {
         $this->shouldHaveType(Command::class);
-    }
-
-    function it_returns_name(): void
-    {
-        $this->name()->shouldBeLike(new Name('King of Hop'));
-    }
-
-    function it_returns_abv(): void
-    {
-        $this->abv()->shouldBeLike(new Abv(5));
     }
 }
--- spec/Application/Command/RegisterConnoisseurSpec.php
+++ spec/Application/Command/RegisterConnoisseurSpec.php
@@ -12,32 +12,21 @@ use Prooph\Common\Messaging\Command;
 
 final class RegisterConnoisseurSpec extends ObjectBehavior
 {
-    function let(): void
+    function it_represents_register_connoisseur_intention(): void
     {
         $this->beConstructedThrough('create', [
             new Name('Krzysztof Krawczyk'),
             new Email('krawczyk@biale.pl'),
             new Password('$2a$04$N2x1MTIgy8fth66TdWZ1NeHIjJIrK7Ns09I9xk1PDRn8IqkQSckua'),
         ]);
+
+        $this->name()->shouldBeLike(new Name('Krzysztof Krawczyk'));
+        $this->email()->shouldBeLike(new Email('krawczyk@biale.pl'));
+        $this->password()->shouldBeLike(new Password('$2a$04$N2x1MTIgy8fth66TdWZ1NeHIjJIrK7Ns09I9xk1PDRn8IqkQSckua'));
     }
 
     function it_is_command(): void
     {
         $this->shouldHaveType(Command::class);
-    }
-
-    function it_has_connoisseur_name(): void
-    {
-        $this->name()->shouldBeLike(new Name('Krzysztof Krawczyk'));
-    }
-
-    function it_has_connoisseur_email(): void
-    {
-        $this->email()->shouldBeLike(new Email('krawczyk@biale.pl'));
-    }
-
-    function it_has_connoisseur_password(): void
-    {
-        $this->password()->shouldBeLike(new Password('$2a$04$N2x1MTIgy8fth66TdWZ1NeHIjJIrK7Ns09I9xk1PDRn8IqkQSckua'));
     }
 }

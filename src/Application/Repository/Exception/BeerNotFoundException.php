d88f914f5f126050837047df89973f0e943fc349
Łukasz Chruściel <lchrusciel@gmail.com>
2018-01-07 14:27:21 +0100

    [Application] Fix exception inheritance

--- src/Application/Repository/Exception/BeerNotFoundException.php
+++ src/Application/Repository/Exception/BeerNotFoundException.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Application\Repository\Exception;
 
-use Zend\EventManager\Exception\DomainException;
-
-final class BeerNotFoundException extends DomainException
+final class BeerNotFoundException extends \DomainException
 {
 }

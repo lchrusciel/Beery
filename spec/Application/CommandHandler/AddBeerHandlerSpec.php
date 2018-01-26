006c0a4a8220cf1b8e577d4293f6bf6bdbc1d3b1
Łukasz Chruściel <lchrusciel@gmail.com>
2018-01-26 19:53:12 +0100

    [Maintenance] Improve specs

--- spec/Application/CommandHandler/AddBeerHandlerSpec.php
+++ spec/Application/CommandHandler/AddBeerHandlerSpec.php
@@ -6,14 +6,12 @@ namespace spec\App\Application\CommandHandler;
 
 use App\Application\Command\AddBeer;
 use App\Application\CommandHandler\AddBeerHandler;
-use App\Application\Event\BeerAdded;
 use App\Application\Repository\Beers;
 use App\Domain\Beer\Model\Abv;
 use App\Domain\Beer\Model\Beer;
 use App\Domain\Beer\Model\Id;
 use App\Domain\Beer\Model\Name;
 use PhpSpec\ObjectBehavior;
-use Prooph\ServiceBus\EventBus;
 use Prophecy\Argument;
 
 final class AddBeerHandlerSpec extends ObjectBehavior
@@ -30,26 +28,16 @@ final class AddBeerHandlerSpec extends ObjectBehavior
 
     function it_creates_a_beer(Beers $beers): void
     {
-        $beers->add(Argument::type(Beer::class))->shouldBeCalled();
+        $beers->add(Argument::that(
+            function (Beer $beer) {
+                return $beer->id() == new Id('e8a68535-3e17-468f-acc3-8a3e0fa04a59');
+            })
+        )->shouldBeCalled();
 
         $this(AddBeer::create(
             new Id('e8a68535-3e17-468f-acc3-8a3e0fa04a59'),
             new Name('King of Hop'),
             new Abv(5)
         ));
-    }
-
-    function it_does_not_dispatch_event_if_adding_beer_would_fail(EventBus $eventBus, Beers $beers): void
-    {
-        $beers->add(Argument::any())->willThrow(\InvalidArgumentException::class);
-
-        $this
-            ->shouldThrow(\InvalidArgumentException::class)
-            ->during('__invoke', [AddBeer::create(
-                new Id('e8a68535-3e17-468f-acc3-8a3e0fa04a59'),
-                new Name('King of Hop'),
-                new Abv(5)
-            )])
-        ;
     }
 }
